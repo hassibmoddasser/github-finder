07c89ba41b214b4a29f41e02f1c044dcf9e7de7c
hassibmoddasser <hassib.moddasser@gmail.com>
2021-10-03 15:01:30 +0430

    Add Footer.js component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Footer from './components/layout/Footer';
 
 class App extends Component {
 
@@ -22,15 +23,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
-        
-        <div className="container mx-auto px-6 pb-12">
-          <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8">
-            <Users 
-              users={this.state.users} 
-              loading={this.state.loading}
-            />
-          </div>
+        <div className="flex flex-col h-screen justify-between">
+          <Navbar />
+          
+          <main>
+            <div className="container mx-auto px-6 pb-12">
+              <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8">
+                <Users 
+                  users={this.state.users} 
+                  loading={this.state.loading}
+                />
+              </div>
+            </div>
+          </main>
+
+          <Footer />
         </div>
       </div>
     );
