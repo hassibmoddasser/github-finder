53c5ed5eabeb98f9425ab4877a6c21c07346c1b0
hassibmoddasser <hassib.moddasser@gmail.com>
2021-10-03 14:20:52 +0430

    Make HTTP request using Axios and Update state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import Navbar from './components/layout/Navbar';
+import axios from 'axios';
 
+import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 
 class App extends Component {
+
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false });
+  }
 
   render() {
     return (
       <div className="App">
         <Navbar />
         
-        <div className="container mx-auto px-6">
+        <div className="container mx-auto px-6 pb-12">
           <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8">
-            <Users />
+            <Users 
+              users={this.state.users} 
+              loading={this.state.loading}
+            />
           </div>
         </div>
       </div>
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,25 +2,8 @@ import React, { Component, Fragment } from 'react';
 import UserItem from './UserItem';
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        login: 'hassibmoddasser',
-        avatar_url: 'https://avatars.githubusercontent.com/u/29257731?v=4',
-        html_url: 'https://github.com/hassibmoddasser'
-      },
-      {
-        id: '2',
-        login: 'monirakarimi',
-        avatar_url: 'https://avatars.githubusercontent.com/u/42800208?v=4',
-        html_url: 'https://github.com/monirakarimi'
-      }
-    ]
-  };
-
   render() {
-    const { users } = this.state;
+    const { users } = this.props;
     
     return (
       <Fragment>
