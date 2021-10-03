7160b75262da1115786caab2b4e877ffa369010c
hassibmoddasser <hassib.moddasser@gmail.com>
2021-10-03 13:53:12 +0430

    Update UserItem & Navbar components to stateless functional components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,61 +1,56 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import { FaGithub } from 'react-icons/fa';
 
-class Navbar extends Component {
-
-  static defaultProps = {
-    title: 'GitHub Finder'
-  };
-
-  static propTypes = {
-    title: PropTypes.string.isRequired
-  };
-  
-  render() {
-    const { title } = this.props;
-
-    return (
-      <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
-        <div className="container mx-auto">
-          <div className="flex-none px-2 mx-2">
-            <FaGithub className="inline text-3xl pr-2" />
-            <span className="text-lg font-bold align-middle">{title}</span>
-          </div> 
-          <div className="flex-1 px-2 mx-2">
-            <div className="items-stretch hidden lg:flex">
-              <a className="btn btn-ghost btn-sm rounded-btn">Home</a> 
-              <a className="btn btn-ghost btn-sm rounded-btn">Portfolio</a> 
-              <a className="btn btn-ghost btn-sm rounded-btn">About</a> 
-              <a className="btn btn-ghost btn-sm rounded-btn">Contact</a>
-            </div>
-          </div> 
-          <div className="flex-none">
-            <button className="btn btn-square btn-ghost">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current">              
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>            
-              </svg>
-            </button>
-          </div> 
-          <div className="flex-none">
-            <button className="btn btn-square btn-ghost">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current">             
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>             
-              </svg>
-            </button>
+function Navbar({ title }) {
+  return (
+    <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
+      <div className="container mx-auto">
+        <div className="flex-none px-2 mx-2">
+          <FaGithub className="inline text-3xl pr-2" />
+          <span className="text-lg font-bold align-middle">{title}</span>
+        </div> 
+        <div className="flex-1 px-2 mx-2">
+          <div className="items-stretch hidden lg:flex">
+            <a className="btn btn-ghost btn-sm rounded-btn">Home</a> 
+            <a className="btn btn-ghost btn-sm rounded-btn">Portfolio</a> 
+            <a className="btn btn-ghost btn-sm rounded-btn">About</a> 
+            <a className="btn btn-ghost btn-sm rounded-btn">Contact</a>
           </div>
-          <div className="flex-none pl-2">
-            <div className="avatar">
-              <div className="rounded-full w-10 h-10 m-1">
-                <img src="https://avatars.githubusercontent.com/u/29257731?v=4" alt="Logged in user" />
-              </div>
+        </div> 
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current">              
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>            
+            </svg>
+          </button>
+        </div> 
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current">             
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>             
+            </svg>
+          </button>
+        </div>
+        <div className="flex-none pl-2">
+          <div className="avatar">
+            <div className="rounded-full w-10 h-10 m-1">
+              <img src="https://avatars.githubusercontent.com/u/29257731?v=4" alt="Logged in user" />
             </div>
           </div>
         </div>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
 }
+
+Navbar.defaultProps = {
+  title: 'GitHub Finder'
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired
+};
 
 export default Navbar;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,27 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types'
 
-class UserItem extends Component {
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
-
-    return (
-      <div className="card shadow-lg compact side bg-base-100">
-        <div className="flex-row items-center space-x-4 card-body">
-          <div>
-            <div className="avatar">
-              <div className="rounded-full w-14 h-14 shadow">
-                <img src={avatar_url} alt="Profile" />
-              </div>
+function UserItem({ user: { login, avatar_url, html_url } }) {
+  return (
+    <div className="card shadow-lg compact side bg-base-100">
+      <div className="flex-row items-center space-x-4 card-body">
+        <div>
+          <div className="avatar">
+            <div className="rounded-full w-14 h-14 shadow">
+              <img src={avatar_url} alt="Profile" />
             </div>
           </div>
-          <div>
-            <h2 className="card-title">{login}</h2>
-            <a className="text-base-content text-opacity-40" href={html_url}>Visit Profile</a>
-          </div>
+        </div>
+        <div>
+          <h2 className="card-title">{login}</h2>
+          <a className="text-base-content text-opacity-40" href={html_url}>Visit Profile</a>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+UserItem.prototype = {
+  user: PropTypes.object.isRequired
+};
 
 export default UserItem;
