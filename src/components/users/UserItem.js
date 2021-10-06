2f729aa24f3316d677d54a4208c04adfd01c75d9
hassibmoddasser <hassib.moddasser@gmail.com>
2021-10-06 23:41:19 +0430

    Add target attribute to "Visit Profile" link to open the user profile in new tab

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -3,18 +3,18 @@ import PropTypes from 'prop-types'
 
 function UserItem({ user: { login, avatar_url, html_url } }) {
   return (
-    <div className="card shadow-lg compact side bg-base-100">
+    <div className="shadow-lg card compact side bg-base-100">
       <div className="flex-row items-center space-x-4 card-body">
         <div>
           <div className="avatar">
-            <div className="rounded-full w-14 h-14 shadow">
+            <div className="rounded-full shadow w-14 h-14">
               <img src={avatar_url} alt="Profile" />
             </div>
           </div>
         </div>
         <div>
           <h2 className="card-title">{login}</h2>
-          <a className="text-base-content text-opacity-40" href={html_url}>Visit Profile</a>
+          <a className="text-base-content text-opacity-40" href={html_url} target="_blank">Visit Profile</a>
         </div>
       </div>
     </div>
