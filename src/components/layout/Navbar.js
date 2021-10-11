d9f4bcc70fc0cd564318b37f3b9f57255439864c
hassibmoddasser <hassib.moddasser@gmail.com>
2021-10-12 00:19:59 +0430

    Refactor Navbar component

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -17,20 +17,6 @@ function Navbar({ title }) {
             <Link className="btn btn-ghost btn-sm rounded-btn" to='/'>Home</Link> 
             <Link className="btn btn-ghost btn-sm rounded-btn" to='/about'>About</Link>
           </div>
-        </div> 
-        <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current">              
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>            
-            </svg>
-          </button>
-        </div> 
-        <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current">             
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>             
-            </svg>
-          </button>
         </div>
         <div className="flex-none pl-2">
           <div className="avatar">
